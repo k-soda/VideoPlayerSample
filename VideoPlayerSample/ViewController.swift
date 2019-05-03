2872a51bec0d40f7ffbc41946822a5c3aa6e5604
k-soda <>
2019-05-03 10:08:40 +0900

    Initial Commit

---
+++ VideoPlayerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerSample
+//
+//  Created by KS on 2019/05/03.
+//  Copyright © 2019 KS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
