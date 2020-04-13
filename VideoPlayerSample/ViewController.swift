e1ad893e265650a14be6413c3c49805d16b2c904
k-soda <>
2020-04-13 20:24:55 +0900

    Add a feature for playing videos

--- VideoPlayerSample/ViewController.swift
+++ VideoPlayerSample/ViewController.swift
@@ -6,11 +6,46 @@
 //  Copyright © 2019 KS. All rights reserved.
 //
 
+import AVKit
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func didTapButton() {
+        guard let path = Bundle.main.path(forResource: "file_example_MP4_480_1_5MG", ofType:"mp4") else {
+            debugPrint("Video not found")
+            return
+        }
+        let fileUrl = URL(fileURLWithPath: path)
+
+        VideoPlayer.playVideo(fileUrl: fileUrl, baseViewController: self)
+    }
+}
+
+final class VideoPlayer {
+
+    static func playVideo(fileUrl: URL, baseViewController: UIViewController) {
+        let player = AVPlayer(url: fileUrl)
+        let playerController = AVPlayerViewController()
+
+        playerController.player = player
+
+        baseViewController.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+
+    static func playVideo(fileUrl: URL, baseView: UIView) {
+        let player = AVPlayer(url: fileUrl)
+        let playerLayer = AVPlayerLayer(player: player)
+
+        playerLayer.frame = baseView.bounds
+        baseView.layer.addSublayer(playerLayer)
+
+        player.play()
     }
 }
