ec8849c5543014bf1279c6fe2b1f639060fc060b
k-soda <>
2020-04-13 21:12:00 +0900

    Create a target of 'VideoPlayerSampleSwiftUI'

---
+++ VideoPlayerSampleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayerSampleSwiftUI
+//
+//  Created by KS on 2020/04/13.
+//  Copyright © 2020 KS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
